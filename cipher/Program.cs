2a2a45e6013480e7819073258f5326ad5511d337
tastytae <73258999+tastytae@users.noreply.github.com>
2020-10-28 00:01:37 +0200

    4th and 5th unit tests
    Creation of fourth and fifth UnitTests. The tests check whether the functions can encrypt and decrypt a text when a negative shift is applied.

--- cipher/Program.cs
+++ cipher/Program.cs
@@ -7,15 +7,30 @@ namespace cipher
         // Creating Caesar cipher - encryption
         public string caesar_encrypt(string text, int shift)
         {
+            int shift_aA = shift, shift_1 = shift;
+            if (shift <= 0)
+            {
+                shift_aA = (shift % 26+26)%26;
+                shift_1 = (shift % 32+32)%32;
+            }
             char[] buffer = text.ToCharArray();
             for(int i = 0; i < buffer.Length; i++)
             {
                 char let = buffer[i];
                 if (let == ' ') continue; // passes on a space symbol
                 // Checks whether the symbol is a upper / lower case letter or a symbol or a number and acts accordingly
-                if ((let <= 'z') && (let >= 'a')) let = (char)((let - 'a' + shift) % 26 + 'a');
-                else if ((let >= 'A') && (let <= 'Z')) let = (char)((let - 'A' + shift) % 26 + 'A');
-                else if ((let >= '!') && (let <= '@')) let = (char)((let - '!' + shift) % 32 + '!');
+                if (char.IsLower(let))
+                {
+                    let = (char)((let - 'a' + shift_aA) % 26 + 'a');
+                }
+                else if (char.IsUpper(let))
+                {
+                    let = (char)((let - 'A' + shift_aA) % 26 + 'A');
+                }
+                else if ((let >= '!') && (let <= '@'))
+                {
+                    let = (char)((let - '!' + shift_1) % 32 + '!');
+                }
                 buffer[i] = let;
             }
             return new string(buffer);
@@ -30,8 +45,8 @@ namespace cipher
                 if (let == ' ') continue; // passes on a space symbol
                 // Checks whether the symbol is a upper / lower case letter or a symbol or a number and acts accordingly
                 // Redefined modulus operation (using modulus operator twice) so that the modulus can work with negative numbers
-                if ((let <= 'z') && (let >= 'a')) let = (char)((((let - 'a' - shift) % 26) + 26) % 26 + 'a');
-                else if ((let <= 'Z') && (let >= 'A')) let = (char)((((let - 'A' - shift) % 26) + 26) % 26 + 'A');
+                if (char.IsLower(let)) let = (char)((((let - 'a' - shift) % 26) + 26) % 26 + 'a');
+                else if (char.IsUpper(let)) let = (char)((((let - 'A' - shift) % 26) + 26) % 26 + 'A');
                 else if ((let <= '@') && (let >= '!')) let = (char)((((let - '!' - shift) % 32) + 32) % 32 + '!');
                 buffer[i] = let;
             }
@@ -39,10 +54,10 @@ namespace cipher
         }
         static void Main(string[] args)
         {
-            string a = "This is a sentence! Greetings, everyone.";
+            string a = "I am an Encryption Bot. Hi! 1 5 90";
             Program pr = new Program();
-            Console.WriteLine(pr.caesar_encrypt(a,20) + " -> encrypted text.");
-            Console.WriteLine(pr.caesar_decrypt(pr.caesar_encrypt(a, 20), 20) + " -> decrypted text.");
+            Console.WriteLine(pr.caesar_encrypt(a,-50) + " -> encrypted text.");
+            Console.WriteLine(pr.caesar_decrypt(pr.caesar_encrypt(a,-50), -50) + " -> decrypted text.");
         }
     }
 }
--- cipherTests/ProgramTests.cs
+++ cipherTests/ProgramTests.cs
@@ -10,13 +10,16 @@ namespace cipher.Tests_for_encryption_decryption
     public class ProgramTests
     {
         [TestMethod()]
-        public void caesar_encrypt_Test()
+        public void caesar_encrypt_Test1()
         {
-            //______________USING AAA PATTERN, TEXT ENCRYPTION
+            //______________USING AAA PATTERN, TEXT ENCRYPTION, POSITIVE SHIFT
             // Arrange - variables for the test
+            /*string x = "this is a sentence";
+            int shift = -20;
+            string expected = "znoy oy g yktzktik";*/
             string x = "this is a sentence";
-            int shift = 5;
-            string expected = "ymnx nx f xjsyjshj";
+            int shift = 28;
+            string expected = "vjku ku c ugpvgpeg";
             // Act - gets the encrypted variable from the encryption function
             var sut = new cipher.Program();
             var encrypted = sut.caesar_encrypt(x, shift);
@@ -26,11 +29,11 @@ namespace cipher.Tests_for_encryption_decryption
         [TestMethod()]
         public void caesar_encrypt_Test2()
         {
-            //______________USING AAA PATTERN, TEXT ENCRYPTION
+            //______________USING AAA PATTERN, TEXT ENCRYPTION, POSITIVE SHIFT
             // Arrange - variables for the test
             string x = "This is a sentence! Greetings, everyone.";
-            int shift = 20;
-            string expected = "Nbcm cm u myhnyhwy5 Alyyncham@ ypylsihy\"";
+            int shift = 15;
+            string expected = "Iwxh xh p htcitcrt0 Vgttixcvh; tktgndct=";
             // Act - gets the encrypted variable from the encryption function
             var sut = new cipher.Program();
             var encrypted = sut.caesar_encrypt(x, shift);
@@ -38,12 +41,40 @@ namespace cipher.Tests_for_encryption_decryption
             Assert.AreEqual(expected, encrypted, "The text was encrypted incorrectly.");
         }
         [TestMethod()]
-        public void caesar_decrypt_Test3()
+        public void caesar_encrypt_Test3()
         {
-            //______________USING AAA PATTERN, TEXT ENCRYPTION
+            //______________USING AAA PATTERN, TEXT ENCRYPTION, NEGATIVE SHIFT
             // Arrange - variables for the test
-            string x = "W oa ob Sbqfmdhwcb Pch< Vw/ ? # '>";
-            int shift = 14;
+            string x = "This is a sentence! Greetings, everyone.";
+            int shift = -25;
+            string expected = "Uijt jt b tfoufodf( Hsffujoht3 fwfszpof5";
+            // Act - gets the encrypted variable from the encryption function
+            var sut = new cipher.Program();
+            var encrypted = sut.caesar_encrypt(x, shift);
+            // Asert - checks if variables are equal, if not - throws an error
+            Assert.AreEqual(expected, encrypted, "The text was encrypted incorrectly.");
+        }
+        [TestMethod()]
+        public void caesar_decrypt_Test4()
+        {
+            //______________USING AAA PATTERN, TEXT ENCRYPTION, POSITIVE SHIFT
+            // Arrange - variables for the test
+            string x = "S kw kx Oxmbizdsyx Lyd8 Rs+ ; ? #:";
+            int shift = 10;
+            string expected = "I am an Encryption Bot. Hi! 1 5 90";
+            // Act - gets the decrypted variable from the decryption function
+            var sut = new cipher.Program();
+            var decrypted = sut.caesar_decrypt(x, shift);
+            // Asert - checks if variables are equal, if not - throws an error
+            Assert.AreEqual(expected, decrypted, "The text was decrypted incorrectly.");
+        }
+        [TestMethod()]
+        public void caesar_decrypt_Test5()
+        {
+            //______________USING AAA PATTERN, TEXT ENCRYPTION, NEGATIVE SHIFT
+            // Arrange - variables for the test
+            string x = "Q iu iv Mvkzgxbqwv Jwb< Pq/ ? # '>";
+            int shift = -18;
             string expected = "I am an Encryption Bot. Hi! 1 5 90";
             // Act - gets the decrypted variable from the decryption function
             var sut = new cipher.Program();
